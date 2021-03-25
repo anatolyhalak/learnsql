90d341f0cd31ba668f7048a5c3fd5561ff31c683
Anatoly <47247410+scsiiq@users.noreply.github.com>
2021-03-25 20:54:35 +0300

    Добавление новых файлов

---
+++ js/main.js
@@ -0,0 +1,177 @@
+/* Initialize */
+jQuery(document).ready(function ($) {
+
+    //подключаем анимацию кнопки бургера
+    $("#top_menu .burgers .menu_btn").click(function () {
+        $("#top_menu .burgers .menu_btn").toggleClass("menu_btn_active");
+    });
+
+    $("#top_menu .bottons .menu_btn").click(function () {
+        $("#top_menu .bottons .menu_btn").toggleClass("menu_btn_active");
+    });
+
+    // меняем "Показать все" на слово "Скрыть" при срабатывании коллапса
+    $(document).on('click', '#show_all', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+    $(document).on('click', '#show_all1', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+    $(document).on('click', '#show_all2', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+    $(document).on('click', '#show_all3', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+    $(document).on('click', '#show_all4', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+    $(document).on('click', '#show_all5', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+    $(document).on('click', '#show_all6', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+    $(document).on('click', '#show_all8', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+    $(document).on('click', '#show_all9', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+    $(document).on('click', '#show_all10', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+    $(document).on('click', '#show_all11', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+    $(document).on('click', '#show_all12', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+    $(document).on('click', '#show_all13', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+    $(document).on('click', '#show_all14', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+    $(document).on('click', '#show_all15', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+    $(document).on('click', '#show_all16', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+    $(document).on('click', '#show_all17', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+    $(document).on('click', '#show_all18', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+    $(document).on('click', '#show_all19', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+    $(document).on('click', '#show_all20', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+    $(document).on('click', '#show_all21', function () {
+        if ($(this).hasClass('collapsed')) {
+            $(this).text('Показать решение');
+        } else {
+            $(this).text('Скрыть');
+        }
+    });
+
+
+
+
+});
